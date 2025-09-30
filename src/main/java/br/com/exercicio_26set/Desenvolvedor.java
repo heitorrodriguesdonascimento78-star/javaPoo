d8bf46735ae633b2b4c7b570c16959c785f84afe
Heitor Rodrigues <heitorrodriguesdonascimento78@gmail.com>
2025-09-30 19:04:00 -0300

    projeto Pooparte2

---
+++ src/main/java/br/com/exercicio_26set/Desenvolvedor.java
@@ -0,0 +1,23 @@
+package br.com.exercicio_26set;
+
+public class Desenvolvedor extends Funcionario{
+
+    private int horasExtras;
+    private Double valorHoraExtra;
+
+    public Desenvolvedor(String nome,String matricula,Double salarioBase, int horasExtras, Double valorHoraExtra){
+        super(nome,matricula,salarioBase);
+        this.horasExtras = horasExtras;
+        this.valorHoraExtra = valorHoraExtra;
+    }
+
+
+    @Override
+    public double calcularSalario(){
+        return salarioBase +(valorHoraExtra * horasExtras);
+    }
+
+    public void registraHorasExtra(int horasExtra){
+        this.horasExtras += horasExtra;
+    }
+}
---
+++ src/main/java/br/com/exercicio_26set/Funcionario.java
@@ -0,0 +1,46 @@
+package br.com.exercicio_26set;
+
+public abstract class Funcionario {
+    protected String nome;
+    protected String matricula;
+    protected Double salarioBase;
+
+    public Funcionario(String nome, String matricula, Double salarioBase) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public Double getSalarioBase() {
+        return salarioBase;
+    }
+    public void setSalarioBase(Double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    // Método abstrato para ser implementado nas subclasses
+    public abstract double calcularSalario();
+
+    // método abstrato deve ser implementado pelas subclasses
+    public void exibirDados(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Matricula: " + matricula);
+        System.out.println("Salario Base: " + salarioBase);
+        System.out.println("Salário Final R$ "+calcularSalario());
+    }
+
+    public void registrarHoraExtra(int i) {
+    }
+}
